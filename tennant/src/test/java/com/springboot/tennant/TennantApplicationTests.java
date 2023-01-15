54a64a1e73dd9b4bf9febc8e1a82d60ed225c2d3
bkoubik88 <benjamin.koubik@gmx.de>
2023-01-15 21:06:53 +0100

    Full Version

---
+++ tennant/src/test/java/com/springboot/tennant/TennantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.tennant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
